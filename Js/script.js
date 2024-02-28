f6909deeaa12d6669bd0142d18cfec51bde6a892
dindaputri1356 <137753900+dindaputri1356@users.noreply.github.com>
2024-02-28 22:56:55 +0700

    delete

--- Js/script.js
+++
@@ -1,8 +0,0 @@
-const menuToggel = document.querySelector('.menu-toggel input');
-const nav = document.querySelector('nav ul');
-
-menuToggel.addEventListener('click',function(){
-    nav.classList.toggle('slide');
-});
-
-
